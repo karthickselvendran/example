8a93a2595938f2a6cab8c1e5a2abed09d323687c
Karthick S <selvendrankarthick@gmail.com>
2020-02-15 14:05:29 +0530

    Updates

---
+++ precache-manifest.c15ece999f189d44f73d51308947f6df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4995a008439e3b41f07e98e42ccf072",
+    "url": "/example/index.html"
+  },
+  {
+    "revision": "f7336f205f8878fa3bff",
+    "url": "/example/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7ade2966325f252ff961",
+    "url": "/example/static/js/2.e633860e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/example/static/js/2.e633860e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7336f205f8878fa3bff",
+    "url": "/example/static/js/main.336ba5c7.chunk.js"
+  },
+  {
+    "revision": "2721cd72fef841cf8ca9",
+    "url": "/example/static/js/runtime-main.97715b68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/example/static/media/logo.5d5d9eef.svg"
+  }
+]);
